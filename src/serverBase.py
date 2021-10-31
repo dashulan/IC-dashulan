62af3bd8e849ba9d1e0fe8691163d666c741f6a8
dashulan <809231301@qq.com>
2021-10-31 15:44:05 +0800

    chage calculate proj into client

--- src/serverBase.py
+++ src/serverBase.py
@@ -58,7 +58,7 @@ class ServerBase:
                 self.sum_parameters = None
                 for client in self.clients:
 
-                    local_parameters = client.localUpdate(1,64,self.model,self.loss_func,self.opti,self.global_parameters,self.proj,tid)
+                    local_parameters = client.localUpdate(1,64,self.model,self.loss_func,self.opti,self.global_parameters,tid)
 
                     if self.sum_parameters is None:
                         self.sum_parameters = {}
@@ -72,8 +72,9 @@ class ServerBase:
                     self.global_parameters[var] = (self.sum_parameters[var]/len(self.clients))
                 self.serverEvaluate(tid)
             
-            
-            self.getProj()
+            for client in self.clients:
+                client.getProj(self.model,self.global_parameters)
+            # self.getProj()
             
             print("-"*40)
             print(f"after Task {tid}")
@@ -250,6 +251,9 @@ class ClientBase:
             "batchsize": 50,
         }
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.feature_list = []
+        self.proj = []
+        self.threshold = np.array([0.965] * 20)
 
     def load_data(self, trainDataset):
         self.train_dataset = trainDataset
@@ -262,7 +266,6 @@ class ClientBase:
         lossFun,
         optim: optim.Optimizer,
         global_parameters,
-        proj_mat:List,
         taskid
     ):
         model.load_state_dict(global_parameters, strict=True)
@@ -277,7 +280,7 @@ class ClientBase:
                 preds = model(data)
                 loss = lossFun(preds[taskid], label)
                 loss.backward()
-                if proj_mat:
+                if self.proj:
                     # for  k,(m,params) in enumerate(model.named_parameters()):
                     #     sz = params.grad.data.size(0)
                     #     params.grad.data = params.grad.data - torch.mm(params.grad.data.view(sz,-1),proj_mat[k]).view(params.size())
@@ -286,7 +289,7 @@ class ClientBase:
                         if k<15 and len(params.size())!=1:
                             sz =  params.grad.data.size(0)
                             params.grad.data = params.grad.data - torch.mm(params.grad.data.view(sz,-1),\
-                                                                    proj_mat[kk]).view(params.size())
+                                                                    self.proj[kk]).view(params.size())
                             kk +=1
                         elif (k<15 and len(params.size())==1) :
                             params.grad.data.fill_(0)
@@ -295,30 +298,75 @@ class ClientBase:
 
         return model.state_dict()
     
+    
+    def getProj(self,model,global_parameters):
+        repr_mat = self.get_representation_matrix(model,global_parameters)
+        self.feature_list = self.updateGPM(self.threshold,repr_mat,self.feature_list)
+        self.proj = self.calculateProj()
+
+    def calculateProj(self):
+        projection_mat = []
+        # for i in range(len(self.model.act)):
+        #     Up = torch.Tensor(np.dot(self.feature_list[i],self.feature_list[i].transpose())).to(self.device)
+        #     projection_mat.append(Up)
+                    # # Projection Matrix Precomputation
+        for i in range(len(self.feature_list)):
+            Uf=torch.Tensor(np.dot(self.feature_list[i],self.feature_list[i].transpose())).to(self.device)
+            print('Layer {} - Projection Matrix shape: {}'.format(i+1,Uf.shape))
+            projection_mat.append(Uf)
+        return projection_mat
+
     def get_representation_matrix(self,model:ResNet,global_parameters):
         model.load_state_dict(global_parameters, strict=True)
         mat_list= model.get_representation_matrix_ResNet18(self.device,self.train_dataset)
-
-        # r = np.arange(len(self.train_dl.dataset))
-        # np.random.shuffle(r)
-        # r = torch.LongTensor(r).to(self.device)
-        # size = min(len(self.train_dataset),300)
-        # b = r[:size]
-        # example_data = self.train_dl.dataset[b][0].view(-1,28*28)
-        # example_data = example_data.to(self.device)
-        # example_out = model(example_data)
-        
-        # batch_list=[size]*3
-        # mat_list = []
-        # act_key = list(model.act.keys())
-        
-        # for i in range(len(act_key)):
-        #     bsz = batch_list[i]
-        #     act = model.act[act_key[i]].detach().cpu().numpy()
-        #     activation = act[0:bsz].transpose()
-        #     mat_list.append(activation)
-            
+        self.repr_matrix = mat_list
         return mat_list
+    
+
+    def updateGPM(self,threshold,mat_list,feature_list=[]):
+
+        if not feature_list:
+            for i in range(len(mat_list)):
+                activation = mat_list[i]
+                U,S,vh = np.linalg.svd(activation,full_matrices=False)
+                sval_total = (S**2).sum()
+                sval_ration = (S**2)/sval_total
+                r= np.sum(np.cumsum(sval_ration)<threshold[i])
+                feature_list.append(U[:,0:r])
+        else:
+            for i in range(len(mat_list)):
+                activation = mat_list[i]
+                U1,S1,vh1 = np.linalg.svd(activation,full_matrices=False)
+                sval_total = (S1**2).sum()
+
+
+                act_hat = activation - np.dot(np.dot(feature_list[i],feature_list[i].transpose()),activation)
+                U,S,Vh = np.linalg.svd(act_hat,full_matrices=False)
+                
+                sval_hat = (S**2).sum()
+                sval_ration = (S**2)/sval_total
+                accumulated_sval = (sval_total-sval_hat)/sval_total
+                
+                r = 0
+
+                for ii in range(sval_ration.shape[0]):
+                    if accumulated_sval<threshold[i]:
+                        accumulated_sval+=sval_ration[ii]
+                        r+=1
+                    else:
+                        break
+                if r==0:
+                    print(f"Skip Updating GPM for layer: {i+1}")
+                    continue
+                
+                Ui = np.hstack((feature_list[i],U[:,0:r]))
+                if Ui.shape[1]>Ui.shape[0]:
+                    feature_list[i]=Ui[:,0:Ui.shape[0]]
+                else:
+                    feature_list[i] = Ui
+        return feature_list
+                
+
 
 
 
